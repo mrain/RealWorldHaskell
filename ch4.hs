8a6fc22dc574b824c33f3f4f19a3bf74af5fee87
MRain <linmrain@gmail.com>
2015-05-12 21:06:38 +0800

    chapter 4 exercises

--- ch4.hs
+++ ch4.hs
@@ -1,4 +1,5 @@
 import System.Environment
+import Data.Char(ord)
 
 -- Exercise 1
 safeHead xs = if null xs then Nothing else Just $ head xs
@@ -30,3 +31,24 @@ myTranspose content = unlines . recur $ lines content
           emptyTail str = if null str then "" else tail str
           recur xs = if all null xs then []
                            else (map emptyHead xs):(recur $ (filter (not . null)) (map emptyTail xs))
+
+asInt_fold s = if (head s == '-') then -(copWith $ tail s) else copWith s
+    where toDigit c = let a = (ord c) - (ord '0') in if (elem a [0..9]) then a else error ("not a dight '" ++ [c] ++ "'")
+          copWith s = foldl (\x->(\y->x * 10 + (toDigit y))) 0 s
+
+concat_foldl xs = foldl (++) [] xs
+
+concat_foldr xs = foldr (flip (foldr (:))) [] xs
+
+takeWhile_mine _ [] = []
+takeWhile_mine f (x:xs) = if (f x) then x:(takeWhile_mine f xs) else []
+
+takeWhile_foldr f xs = foldr comb [] xs
+    where comb x ys = if (f x) then x:ys else []
+
+groupBy_fold f xs = foldr comb [] xs
+    where comb x [] = [[x]]
+          comb x (y:ys) = if (f x (head y)) then (x:y):ys else [x]:y:ys
+
+any_fold f xs = foldl (flip ((||) . f)) False xs
+cycle_fold xs = foldl (flip (\x->(++) xs)) [] [1..]
